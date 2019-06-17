8989df5a969d4a8e3b56295d36b7b3c2b249a075
Dmitriy <kontroler.l2@gmail.com>
2019-06-17 22:22:01 +0300

    second commit

--- main.js
+++ main.js
@@ -16,8 +16,8 @@
     Toaster: Тостер,
     Coffee Machine: Кофемашина
 */
-
-let jsonProducts = '[{"category":"TV","price":1500,"manufacturer":"Sony","createdAt":"2019-05-28T17:55:29.945Z"},{"category":"Laptop","price":1200,"manufacturer":"Acer","createdAt":"2019-05-28T19:55:29.946Z"},{"category":"Smartphone","price":750,"manufacturer":"iPhone","createdAt":"2018-03-08T10:45:00.000Z"},{"category":"Fridge","price":1850,"manufacturer":"Vestfrost","createdAt":"2018-05-28T17:55:29.946Z"},{"category":"Boiler","price":500,"manufacturer":"Indesit","createdAt":"2014-12-25T08:30:00.000Z"},{"category":"Stove","price":700,"manufacturer":"Gorenje","createdAt":"2018-09-17T11:00:00.000Z"},{"category":"Washing Machine","price":850,"manufacturer":"Electrolux","createdAt":"2019-05-28T18:55:29.946Z"},{"category":"Vacuum Cleaner","price":450,"manufacturer":"Samsung","createdAt":"2019-05-13T17:55:29.946Z"},{"category":"Сonditioner","price":1000,"manufacturer":"Toshiba","createdAt":"2017-07-01T00:00:00.000Z"},{"category":"Iron","price":320,"manufacturer":"Philips","createdAt":"2013-11-18T07:20:00.000Z"},{"category":"Teapot","price":400,"manufacturer":"Bosch","createdAt":"2016-10-03T09:45:00.000Z"},{"category":"Electric Shaver","price":440,"manufacturer":"Braun","createdAt":"2019-05-29T03:55:29.946Z"},{"category":"Toaster","price":620,"manufacturer":"Tefal","createdAt":"2015-05-29T03:55:29.946Z"},{"category":"Coffee Machine","price":1300,"manufacturer":"Delonghi","createdAt":"2019-05-28T02:55:29.946Z"}]';
+const line = '--------------------\n'
+const jsonProducts = '[{"category":"TV","price":1500,"manufacturer":"Sony","createdAt":"2019-05-28T17:55:29.945Z"},{"category":"Laptop","price":1200,"manufacturer":"Acer","createdAt":"2019-05-28T19:55:29.946Z"},{"category":"Smartphone","price":750,"manufacturer":"iPhone","createdAt":"2018-03-08T10:45:00.000Z"},{"category":"Fridge","price":1850,"manufacturer":"Vestfrost","createdAt":"2018-05-28T17:55:29.946Z"},{"category":"Boiler","price":500,"manufacturer":"Indesit","createdAt":"2014-12-25T08:30:00.000Z"},{"category":"Stove","price":700,"manufacturer":"Gorenje","createdAt":"2018-09-17T11:00:00.000Z"},{"category":"Washing Machine","price":850,"manufacturer":"Electrolux","createdAt":"2019-05-28T18:55:29.946Z"},{"category":"Vacuum Cleaner","price":450,"manufacturer":"Samsung","createdAt":"2019-05-13T17:55:29.946Z"},{"category":"Сonditioner","price":1000,"manufacturer":"Toshiba","createdAt":"2017-07-01T00:00:00.000Z"},{"category":"Iron","price":320,"manufacturer":"Philips","createdAt":"2013-11-18T07:20:00.000Z"},{"category":"Teapot","price":400,"manufacturer":"Bosch","createdAt":"2016-10-03T09:45:00.000Z"},{"category":"Electric Shaver","price":440,"manufacturer":"Braun","createdAt":"2019-05-29T03:55:29.946Z"},{"category":"Toaster","price":620,"manufacturer":"Tefal","createdAt":"2015-05-29T03:55:29.946Z"},{"category":"Coffee Machine","price":1300,"manufacturer":"Delonghi","createdAt":"2019-05-28T02:55:29.946Z"}]';
 
 let filterProducts = null;
 
@@ -43,7 +43,7 @@ let filterProducts = null;
 
             // View product list
             case 'a':
-                console.log('[View product list]: ');
+                console.log(line + 'View product list\n' + line);
                 console.table(stringJson);
 
 
@@ -52,7 +52,7 @@ let filterProducts = null;
                 break;
             // Set filters
             case 'b':
-                console.log('[Set filters]');
+                console.log(line + 'Set filters\n' + line);
                 const filtersMenu = prompt('Set Filters:\n' +
                     'a) Category\n' +
                     'b) Price\n' +
@@ -63,17 +63,19 @@ let filterProducts = null;
                 switch (filtersMenu) {
                     // Category
                     case 'a':
-                        stringJson.forEach(element => {
-                            for (let i = 1; i <= stringJson.length; i++) {
-                                console.log(i + ') ' + element.category);
-                            };
+                        console.log('%cSet category:', 'color: green');
+                        const letterIterator = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',]
+                        stringJson.forEach(obj => {
+                            for(let i = 0; i < letterIterator.length; i++)
+                            console.table(letterIterator[i] + ')' + obj.category);
                         });
+                        const category = prompt('Select a category from the console:')
 
                         // (function filter() {
                         //     filterProducts = stringJson.filter()
                         // })();
 
-                        console.log(stringJson);
+                        // console.log(stringJson);
 
                         break;
 
@@ -105,7 +107,7 @@ let filterProducts = null;
             // Sort Products
             case 'c':
 
-                console.log('[Sort Products]');
+                console.log(line + 'Sort Products\n' + line);
 
 
 
@@ -114,7 +116,7 @@ let filterProducts = null;
             // Quit
             case 'q':
                 alert('Come again!')
-                console.log('[Quit]');
+                console.log(line + 'Quit\n' + line);
                 return;
 
             default:
