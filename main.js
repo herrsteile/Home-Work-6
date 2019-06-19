72e3d06f3a2f8dcc40fd9e5adc659f77c2d29f3b
Dmitriy <kontroler.l2@gmail.com>
2019-06-19 10:11:17 +0300

    19.06.2019

--- main.js
+++ main.js
@@ -15,11 +15,11 @@
     Electric Shaver: Электро-бритва,
     Toaster: Тостер,
     Coffee Machine: Кофемашина
-*/
+    */
 const line = '--------------------\n'
 const jsonProducts = '[{"category":"TV","price":1500,"manufacturer":"Sony","createdAt":"2019-05-28T17:55:29.945Z"},{"category":"Laptop","price":1200,"manufacturer":"Acer","createdAt":"2019-05-28T19:55:29.946Z"},{"category":"Smartphone","price":750,"manufacturer":"iPhone","createdAt":"2018-03-08T10:45:00.000Z"},{"category":"Fridge","price":1850,"manufacturer":"Vestfrost","createdAt":"2018-05-28T17:55:29.946Z"},{"category":"Boiler","price":500,"manufacturer":"Indesit","createdAt":"2014-12-25T08:30:00.000Z"},{"category":"Stove","price":700,"manufacturer":"Gorenje","createdAt":"2018-09-17T11:00:00.000Z"},{"category":"Washing Machine","price":850,"manufacturer":"Electrolux","createdAt":"2019-05-28T18:55:29.946Z"},{"category":"Vacuum Cleaner","price":450,"manufacturer":"Samsung","createdAt":"2019-05-13T17:55:29.946Z"},{"category":"Сonditioner","price":1000,"manufacturer":"Toshiba","createdAt":"2017-07-01T00:00:00.000Z"},{"category":"Iron","price":320,"manufacturer":"Philips","createdAt":"2013-11-18T07:20:00.000Z"},{"category":"Teapot","price":400,"manufacturer":"Bosch","createdAt":"2016-10-03T09:45:00.000Z"},{"category":"Electric Shaver","price":440,"manufacturer":"Braun","createdAt":"2019-05-29T03:55:29.946Z"},{"category":"Toaster","price":620,"manufacturer":"Tefal","createdAt":"2015-05-29T03:55:29.946Z"},{"category":"Coffee Machine","price":1300,"manufacturer":"Delonghi","createdAt":"2019-05-28T02:55:29.946Z"}]';
 
-let filterProducts = null;
+const letterIterator = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
 
 (function run() {
     while (true) {
@@ -31,6 +31,13 @@ let filterProducts = null;
             return value;
         });
 
+        function checkLetter(enteredLetter) {
+            for (let i = 0; i <= letterIterator.length; i++) {
+                return enteredLetter === letterIterator[i];
+            };
+        }
+
+        let filterProducts = null;
         filterProducts = stringJson;
 
         const mainMenu = prompt('Main menu:\n' +
@@ -64,18 +71,26 @@ let filterProducts = null;
                     // Category
                     case 'a':
                         console.log('%cSet category:', 'color: green');
-                        const letterIterator = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',]
-                        stringJson.forEach(obj => {
-                            for(let i = 0; i < letterIterator.length; i++)
-                            console.table(letterIterator[i] + ')' + obj.category);
+                        stringJson.forEach((obj, i) => {
+                            console.log((letterIterator[i] + ')' + obj.category));
                         });
-                        const category = prompt('Select a category from the console:')
+                        console.log('-)To select all categories except selected');
 
-                        // (function filter() {
-                        //     filterProducts = stringJson.filter()
-                        // })();
+                        let category = prompt('Select a category from the console:')
+                        let selectedCategories = stringJson.filter((value) => {
+                            let selectedObj = null;
+                            stringJson.forEach(obj => {
+                                selectedObj = obj.category;
+                            });
+                        })
 
-                        // console.log(stringJson);
+                        if (!category.split('').find(checkLetter)) {
+                            console.log('There is no such category');
+                        } else {
+                            console.log('Categories selected');
+                        }
+                        console.log(selectedCategories);
+
 
                         break;
 
